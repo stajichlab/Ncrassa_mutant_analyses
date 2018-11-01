25fe47e271a25645ed5b7fcba73b147cb6d6fa64
Jason Stajich <jasonstajich.phd@gmail.com>
2018-11-01 00:02:25 -0700

    run snpEff

---
+++ pipeline/06_snpEff.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash 
+#SBATCH --mem=16G --nodes 1 --ntasks 2 --out logs/snpEff.log
+
+module load snpEff
+SNPEFFOUT=snpEff
+SNPEFFGENOME=Ncrassa_FungiDB_39
+snpEffConfig=snpEff.config
+GBKGENOME=GCA_000182925.2_NC12_genomic.gbff
+GFFGENOME=FungiDB-39_NcrassaOR74A.gff
+MEM=16g
+
+# this module defines SNPEFFJAR and SNPEFFDIR
+if [ -f config.txt ]; then
+	source config.txt
+fi
+GBKGENOMEFILE=$GENOMEFOLDER/$GBKGENOME
+GFFGENOMEFILE=$GENOMEFOLDER/$GFFGENOME
+FASTAGENOMEFILE=$GENOMEFOLDER/$GENOMEFASTA
+if [ -z $SNPEFFJAR ]; then
+ echo "need to defined \$SNPEFFJAR in module or config.txt"
+ exit
+fi
+if [ -z $SNPEFFDIR ]; then
+ echo "need to defined \$SNPEFFDIR in module or config.txt"
+ exit
+fi
+# could make this a confi
+
+if [ -z $FINALVCF ]; then
+	echo "need a FINALVCF variable in config.txt"
+	exit
+fi
+
+mkdir -p $SNPEFFOUT
+if [ ! -e $SNPEFFOUT/$snpEffConfig ]; then
+	rsync -a $SNPEFFDIR/snpEff.config $SNPEFFOUT/$snpEffConfig
+	echo "# ncrassa.fungidb " >> $SNPEFFOUT/$snpEffConfig
+  	echo "$SNPEFFGENOME.genome : Neurospora crassa FungiDB" >> $SNPEFFOUT/$snpEffConfig
+	#chroms=$(grep LOCUS $GBKGENOMEFILE | awk '{print $2}' | perl -p -e 's/\n/, /' | perl -p -e 's/,\s+$/\n/')
+	chroms=$(grep '##sequence-region' $GFFGENOMEFILE | awk '{print $2}' | perl -p -e 's/\n/, /' | perl -p -e 's/,\s+$/\n/')
+	echo -e "\t$SNPEFFGENOME.chromosomes: $chroms" >> $SNPEFFOUT/$snpEffConfig
+	echo -e "\t$SNPEFFGENOME.KC683708.codonTable : Mold_Mitochondrial" >> $SNPEFFOUT/$snpEffConfig
+	mkdir -p $SNPEFFOUT/data/$SNPEFFGENOME
+	gzip -c $GFFGENOMEFILE > $SNPEFFOUT/data/$SNPEFFGENOME/genes.gff.gz
+	rsync -a $FASTAGENOMEFILE $SNPEFFOUT/data/$SNPEFFGENOME/sequences.fa
+
+#	java -Xmx$MEM -jar $SNPEFFJAR build -datadir `pwd`/$SNPEFFOUT/data -c $SNPEFFOUT/$snpEffConfig -genbank -v $SNPEFFGENOME 
+	java -Xmx$MEM -jar $SNPEFFJAR build -datadir `pwd`/$SNPEFFOUT/data -c $SNPEFFOUT/$snpEffConfig -gff3 -v $SNPEFFGENOME
+fi
+pushd $SNPEFFOUT
+INVCF=../$FINALVCF/$PREFIX.selected.SNP.vcf
+OUTVCF=$PREFIX.snpEff.vcf
+
+java -Xmx$MEM -jar $SNPEFFJAR eff -dataDir `pwd`/data -v $SNPEFFGENOME $INVCF > $OUTVCF
